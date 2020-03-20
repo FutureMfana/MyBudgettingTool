c7452c21ea98acdd81dfc9e95859efcf1dbdd9c8
Kamvelihle Innocent Mjanyelwa <31170359+FutureMfana@users.noreply.github.com>
2020-03-20 09:14:42 +0200

    last pacth

---
+++ SQLQuery1.sql
@@ -0,0 +1,29 @@
+﻿CREATE SCHEMA Bugdet;
+GO
+
+/*CREATE TABLE Budget.UserInfo(
+	UserID INT IDENTITY(1,1) PRIMARY KEY,
+	Salary INT NOT NULL,
+	Campony NVARCHAR(40) NOT NULL,
+	HireDate DATE,
+	Position NVARCHAR(40),
+	Email NVARCHAR(40),
+	Password CHAR(14)
+);
+GO*/
+
+CREATE TABLE MyBudget(
+	ID INT IDENTITY(1,1) PRIMARY KEY,
+	Expenses NVARCHAR(40) NOT NULL,
+	Costs INT NOT NULL,
+	CostsInPercent AS ((Costs / 7302) * 100),
+	Month_ TinyInt NOT NULL,
+	Year_ INT NOT NULL
+);
+GO
+
+CREATE VIEW Budget_TBL
+AS
+	SELECT ID AS [ROWID], Expenses AS [Expense], Costs AS [Total Price], CostsInPercent AS [Total Price In Percent], Month_ AS [Month], Year_ AS [Year]  
+	FROM MyBudget
+GO
